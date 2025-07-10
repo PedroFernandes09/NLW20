15c1c299eb04764ef2a9dca0bab7d7344f3f941b
Pedro Henrique Fernandes <pedrohenriquefernandes296@gmail.com>
2025-07-10 15:55:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ const markdownConverter = (text) => {
   return converter.makeHtml(text)
 }
 
-// AIzaSyCtPzzi3lRvFJjh7RVJSCNyJj8sTWbWo-A
 const perguntarIA = async (question, game, apiKey) => {
  const model = "gemini-2.5-flash"
  const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}` 
